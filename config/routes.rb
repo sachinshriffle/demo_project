8fcfd2cf1fb60265eb7c477dd8de73c24460d740
sachin <sachinp@shriffle.com>
2023-10-03 19:24:15 +0530

    add specific routes for devise sign_out

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  resources :users do
-    get 'user_company', on: :collection
-    get 'forgot_password', on: :collection
-    get 'reset_password', on: :collection
+  root "companies#index"
+#   resources :users do
+#     get 'user_company', on: :collection
+#     get 'forgot_password', on: :collection
+#     get 'reset_password', on: :collection
+#   end
+  devise_scope :user do
+    get '/users/sign_out', to: 'devise/sessions#destroy'
   end
-
-  resources :companies, :job_applications
+  resources :companies
   resources :skills do 
     post 'add_skill', on: :collection
   end
@@ -18,15 +21,15 @@ Rails.application.routes.draw do
     get 'current_company_jobs', on: :collection
   end
 
-  post '/auth/login', to: 'authentication#login'
-  post 'jobs/:job_id/apply', to: 'job_applications#apply'
-  get '/suggested_jobs', to: 'users#suggested_jobs'
-  get '/top_jobs', to: 'jobs#top_jobs'
-  get '/all_applied_jobs', to: 'users#all_applied_jobs'
-  get '/user_skills', to: 'skills#user_skills'
-  get '/specific_job/:id', to: 'users#specific_job'
-  get '/search_jobs_by_company_or_skill_name', to: 'jobs#search_jobs_by_company_name'
-  get '/application_by_status', to: 'job_applications#application_by_status'
-  get '/company_by_job_id', to: 'companies#company_by_job_id' 
-  get '/search', to: 'companies#search'
-end
+#   post '/auth/login', to: 'authentication#login'
+#   post 'jobs/:job_id/apply', to: 'job_applications#apply'
+#   get '/suggested_jobs', to: 'users#suggested_jobs'
+#   get '/top_jobs', to: 'jobs#top_jobs'
+#   get '/all_applied_jobs', to: 'users#all_applied_jobs'
+#   get '/user_skills', to: 'skills#user_skills'
+#   get '/specific_job/:id', to: 'users#specific_job'
+#   get '/search_jobs_by_company_or_skill_name', to: 'jobs#search_jobs_by_company_name'
+#   get '/application_by_status', to: 'job_applications#application_by_status'
+#   get '/company_by_job_id', to: 'companies#company_by_job_id' 
+#   get '/search', to: 'companies#search'
+ end
